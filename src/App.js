c5113e7a53416473cce8ee4826debdd8f1beb3bd
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2017-10-10 21:16:55 +0800

    Adds basic structure for ReactJS application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div><p>Basic App</p><h1>Revise</h1></div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+var config = {
+  entry: "./main.js",
+
+  output: {
+    path: "/",
+    filename: "index.js"
+  },
+
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: [path.resolve("src"), path.resolve(__dirname, "node_modules")]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+
+        query: {}
+      }
+    ]
+  }
+};
+
+module.exports = config;
